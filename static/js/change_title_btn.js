7687cfb3342b3cf1cf8e44239c63c7e23a0cbdc1
qwe <test@mail.ru>
2022-11-24 00:17:06 +0300

    add title btn

---
+++ static/js/change_title_btn.js
@@ -0,0 +1,13 @@
+function change_title_btn_add() {
+
+    let url = window.location.href;
+    if (url.split(["/"])[3] === "article") {
+        let btn_add = document.querySelector("#add");
+        btn_add.innerHTML = "Добавить новость";
+        let btn_back = document.querySelector("#back");
+        btn_back.innerHTML = "Назад к списку новостей";
+    }
+
+}
+
+change_title_btn_add();
